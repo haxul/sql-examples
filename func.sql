620dc53c27f4b119c4568e3c1b65992acb66628d
Sergei Starodubov <41199611+haxul@users.noreply.github.com>
2020-03-22 18:17:43 +0400

    Update func.sql

--- func.sql
+++ func.sql
@@ -1,3 +1,6 @@
+-- function no return
+
+CREATE OR REPLACE FUNCTION fix_user()
 RETURNS void as
 $$
     UPDATE "user"
@@ -16,4 +19,13 @@ CREATE OR REPLACE FUNCTION find_oldest_user()
 
 select find_oldest_user();
 
-	
+-- own data types
+create type address as (
+    street varchar(200),
+    flat_number integer
+                       );
+
+INSERT INTO"user" (id, name, age, address)
+VALUES (100, 'anotherName' , 150 , ROW ('matrosova', 57));
+
+select (address).flat_number as flat_number from "user";
